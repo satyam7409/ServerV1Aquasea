72ca021837ba3f6dbc4a2b089d96e20553d5394f
Aashutosh kumar bahrdwaj <ashutoshkumarbhardwaj7@gmail.com>
2025-09-10 22:57:13 +0530

    working one

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 import express from "express";
 import userroutes from "./src/routes/user.routes.js";
+import cors from "cors";
 
 const app = express();
 // const router = express.Router();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// app.use("/", router);
+app.use(cors());
 
 app.use("/users", userroutes);
+
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
 
 export default app;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,7 @@ const router = Router();
 import { upload } from "../middlewares/multer.middleware.js";
 import { uploadFasta } from "../controllers/user.controller.js";
 
-router.post("/uploads", upload.single("file"), uploadFasta);
+router.post("/upload-fasta", upload.single("file"), uploadFasta);
 
 router.post("/hello", (req, res) => {
   res.send("Hello World!");
